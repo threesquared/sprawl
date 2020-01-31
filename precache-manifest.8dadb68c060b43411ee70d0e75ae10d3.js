a6373db3b082c5ed0fa6c60e9ebe2fceeff0aa65
Ben <ben.speakman@peakon.com>
2020-01-31 20:54:26 +0000

    Updates

--- precache-manifest.8dadb68c060b43411ee70d0e75ae10d3.js
+++ precache-manifest.8dadb68c060b43411ee70d0e75ae10d3.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cd8684c4be4dc2939a8174f6defc3911",
+    "revision": "535bf6fcbb1c76f2fe12b13cc9b4fe83",
     "url": "/sprawl/index.html"
   },
   {
-    "revision": "eaec6d86413054c93c35",
+    "revision": "51d180ccbb33dd0103bf",
     "url": "/sprawl/static/css/main.96504f10.chunk.css"
   },
   {
-    "revision": "074ec99a91e609faf08e",
-    "url": "/sprawl/static/js/2.523cf8c9.chunk.js"
+    "revision": "d6a44a77160b9a780899",
+    "url": "/sprawl/static/js/2.6817f2b5.chunk.js"
   },
   {
     "revision": "3685b63f93ef0f4be08f1e64a5fdb4cb",
-    "url": "/sprawl/static/js/2.523cf8c9.chunk.js.LICENSE"
+    "url": "/sprawl/static/js/2.6817f2b5.chunk.js.LICENSE"
   },
   {
-    "revision": "eaec6d86413054c93c35",
-    "url": "/sprawl/static/js/main.28010ee2.chunk.js"
+    "revision": "51d180ccbb33dd0103bf",
+    "url": "/sprawl/static/js/main.11e01196.chunk.js"
   },
   {
     "revision": "497797cf83e5255adabe",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sprawl/precache-manifest.6b76baae6252cce1e8dfb0722efeaac6.js"
+  "/sprawl/precache-manifest.8dadb68c060b43411ee70d0e75ae10d3.js"
 );
 
 self.addEventListener('message', (event) => {
